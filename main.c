611f8139e5f6ca32da5479ff8ba6dd400a21b477
Adino <kozacek.adrian@gmail.com>
2026-02-02 19:54:12 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+// struktury
+struct Product {
+    char name[50];
+    float price;
+    int quantity;
+};
+
+
+struct Product warehouse[100];
+int productCount = 0;
+
+int main() {
+    return 0;
+}
